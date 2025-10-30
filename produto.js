fcf450552eaf36a2f6e816d457acc7be3d77dfd2
Pedro909r <rodrigues.santos.pedro0506@escola.pr.gov.br>
2025-10-29 21:20:02 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,9 @@
+const NomeDoProduto="camiseta de Algodão";
+let precodoproduto=29.99;
+const marcadoproduto="Marca x"
+const produtocompleto=(NomeDoProduto+":"+"\n"+marcadoproduto+":"+"\n"+precodoproduto);
+precodoproduto=49.99
+console.log(produtocompleto)
+console.log("Produto:", produtocompleto);
+console.log("preço atualizado: R$", precodoproduto);
+NomeDoProduto="camiseta de linho"
